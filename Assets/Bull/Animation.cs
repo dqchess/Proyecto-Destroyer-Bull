0cdef3508cf97ad53237a9e6e914cf65cb1ed91b
FerSO7 <fernando.ochoa.mza@gmail.com>
2019-11-27 21:15:19 -0300

    Test

---
+++ Assets/Bull/Animation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animation : MonoBehaviour
+{
+    private Animation anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animation>();
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
