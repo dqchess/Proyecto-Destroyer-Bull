a95b94e3b3643409330d73f9574427db8cbf9194
FerSO7 <fernando.ochoa.mza@gmail.com>
2019-12-03 02:06:53 -0300

    3/12
    2:06

---
+++ Assets/OpenShop.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenShop : MonoBehaviour
+{
+    public GameObject shop;
+
+    public void activateShop()
+    {
+        shop.SetActive(true);
+    }
+
+    public void deactivateShop()
+    {
+        shop.SetActive(false);
+    }
+}
---
+++ Assets/ScriptableObjects/Rewards.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="Reward",menuName ="New Reward")]
+public class Rewards :ScriptableObject
+{
+    public string name;
+    public int value;
+    public Sprite spriteImage;
+}
