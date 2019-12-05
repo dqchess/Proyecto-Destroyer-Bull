81f4630ac3412aac111b784d099327c9dda27130
FerSO7 <fernando.ochoa.mza@gmail.com>
2019-12-05 06:05:30 -0300

    5/12
    6:05

---
+++ Assets/Scripts/Kinematic.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kinematic : MonoBehaviour
+{
+    Rigidbody rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        rb.isKinematic = false;
+    }
+}
