ffc617a9f0670c82e51dc349fcf80f5e56f903ff
FerSO7 <fernando.ochoa.mza@gmail.com>
2019-12-05 02:22:25 -0300

    Limpieza de proyecto
    tamb creamos scriptables objects

---
+++ Assets/Scripts/Bull.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="Bull",menuName ="New Bull")]
+public class Bull :ScriptableObject
+{
+    public string name;
+    public int shopValue;
+    public GameObject inGameGameObject;
+    public GameObject inStoreGameObject;
+    public Sprite spriteImage;
+    public int value;
+}
---
+++ Assets/Scripts/CanvasGame.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasGame : MonoBehaviour
+{
+    private void Awake()
+    {
+        Time.timeScale = 0; // sacar pausa, poner velocidad del toro a 0 y realizar alguna accion como el fade
+    }
+    public void activateUI(GameObject UI)
+    {
+        UI.SetActive(true);
+    }
+    public void deactivateUI(GameObject UI)
+    {
+        UI.SetActive(false);
+    }
+
+    public void play()
+    {
+        Time.timeScale = 1; // aca restaurariamos la velocidad del toro para empezar
+    }
+}
