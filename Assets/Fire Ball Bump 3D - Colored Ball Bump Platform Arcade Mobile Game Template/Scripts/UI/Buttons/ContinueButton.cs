bff4ed085110bdfa9bced576ce61f3092cd78e92
FerSO7 <fernando.ochoa.mza@gmail.com>
2019-12-20 21:42:55 -0300

    finish2

--- Assets/Fire Ball Bump 3D - Colored Ball Bump Platform Arcade Mobile Game Template/Scripts/UI/Buttons/ContinueButton.cs
+++ Assets/Fire Ball Bump 3D - Colored Ball Bump Platform Arcade Mobile Game Template/Scripts/UI/Buttons/ContinueButton.cs
@@ -7,6 +7,11 @@ public class ContinueButton : MonoBehaviour {
 	//This needs to be attached to the UI Buttons Manager so when it's OnClick is active it will load the next scene.
 
 	public void ContinueGame() {
+        
 		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
 	}
+    public void loadFirstScene()
+    {
+        SceneManager.LoadScene("Scene01-F", LoadSceneMode.Single);
+    }
 }
